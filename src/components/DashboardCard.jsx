4bc82eefae47e26a5a2b6de7fb088f01f8d8110a
Cale <cale.tian@icloud.com>
2022-12-17 16:44:54 -0800

    added status for sent requests

--- src/components/DashboardCard.jsx
+++ src/components/DashboardCard.jsx
@@ -22,7 +22,7 @@ const ActionBar = () => {
 
 const DashboardSection = (props) => {
     return (
-        <div className=" w-full bg-indigo-50 mx-auto rounded-2xl flex flex-col overflow-hidden mt-2 select-none">
+        <div className=" w-full bg-indigo-50 mx-auto rounded-2xl flex flex-col overflow-hidden mt-2 select-none min-h-screen">
             <DashboardHero artistInfo={props.artistInfo} />
             <ActionBar />
             <Routes>
@@ -62,7 +62,6 @@ const DashboardHero = (props) => {
         </>
     )
 }
-
 
 const Requests = (props) => {
     const [modal, setModal] = useState(false)
@@ -109,6 +108,21 @@ const Requests = (props) => {
                 props.setModal(false)
             alert("You have accepted this request!")}
         }
+
+        const deleteRequest = async () => {
+            try {
+                const { data, error } = await supabase
+                .from('requests')
+                .delete()
+                .eq('request_id', eventDetail.request_id)
+                console.log(error)
+            }
+            catch {
+                console.log(error)}
+            finally {
+                props.setModal(false)
+            alert("You have declined this request!")}
+        }
         return (
             <div className="min-h-60">
                 <div className="w-full h-16 bg-indigo-900 flex justify-center items-center font-semibold text-indigo-50 text-xl">Event Detail</div>
@@ -146,7 +160,7 @@ const Requests = (props) => {
                 </div>
                 <div className="flex justify-evenly w-full">
                     <div className="p-4 bg-indigo-900 w-1/2 font-semibold hover:bg-teal-700 text-indigo-50 transition-all cursor-pointer rounded-none text-center" onClick={(e)=> acceptRequest()}>Accept</div>
-                    <div className="p-4 text-center bg-indigo-900 w-1/2 font-semibold hover:bg-rose-700 text-indigo-50 transition-all cursor-pointer">Decline</div>
+                    <div className="p-4 text-center bg-indigo-900 w-1/2 font-semibold hover:bg-rose-700 text-indigo-50 transition-all cursor-pointer" onClick={(e) => deleteRequest()}>Decline</div>
                 </div>
             </div>
         )
@@ -171,7 +185,6 @@ const Requests = (props) => {
         </div>
     )
 }
-
 
 const AcceptedRequests = (props) => {
     const [modal, setModal] = useState(false)
@@ -280,13 +293,12 @@ const SentRequests = (props) => {
                         .map((item) => {
                             const date = new Date(item.request_timestamp)
                             const dateFormatted = date.toLocaleString()
-                            return <RequestCard key={item.request_id} id={item.request_id} date={dateFormatted} location={item.location} detail={item.description} created_by={item.sent_to} text="To" type="outgoing" />
+                            return <RequestCard key={item.request_id} id={item.request_id} date={dateFormatted} location={item.location} detail={item.description} created_by={item.sent_to} approved={item.approved} text="To" type="outgoing" />
                         }) : ""}
             </div>
         </div>
     )
 }
-
 
 const RequestCard = (props) => {
     const [userName, setUserName] = useState('')
@@ -313,8 +325,8 @@ const RequestCard = (props) => {
                 props.setModal(true)
             }
             }>
-            <div className="flex-1 flex-col flex gap-3 w-1/2 justify-center h-full hover:bg-indigo-900 transition-colors p-4 cursor-pointer">
-                <div className="flex flex-col gap-1 justify-center py-5 px-2">
+            <div className="flex-1 flex gap-3 w-1/2 justify-center h-full hover:bg-indigo-900 transition-colors p-4 cursor-pointer">
+                <div className="flex flex-col w-full gap-1 justify-center py-5 px-2">
                     <div className="flex gap-1">
                         <h2 className="font-semibold text-indigo-50 capitalize w-[4.6rem]">{props.text}:</h2>
                         <p className="font-semibold text-indigo-50 capitalize">{userName.name}</p>
@@ -326,8 +338,16 @@ const RequestCard = (props) => {
                     <div className="flex gap-1">
                         <h2 className="font-extralight text-indigo-50 text-sm capitalize w-[4.6rem]">Date:</h2>
                         <p className="font-extralight text-indigo-50 text-sm capitalize"><Timestamp date={props.date} /></p>
-                        
                     </div>
+                    {props.type === "outgoing" &&
+                        <div className="flex gap-1 mt-1 items-center">
+                        <h2 className="font-extralight text-indigo-50 text-sm capitalize w-[4.6rem]">Status:</h2>
+                        {props.approved === true 
+                        ?<div className="border-2 rounded-xl px-2 bg-teal-600 border-teal-600"><p className="font-semibold text-indigo-50 text-sm capitalize">Approved</p></div>
+                        : <div className="border-2 rounded-xl px-2 bg-purple-600 border-purple-600"><p className="font-semibold text-indigo-50 text-sm capitalize">Pending</p></div>
+                        }
+                        </div>
+                    }
                 </div>
             </div>
         </div>
@@ -364,7 +384,7 @@ const Events = (props) => {
                     You have {props.eventInfo ? props.eventInfo.length : "no"} public events.
                 </h1>
             </div>
-            <div className="bg-indigo-50 flex flex-col gap-1 items-center py-1">
+            <div className="bg-indigo-50 flex flex-col gap-1 items-center">
                 {props.eventInfo !== null ? props.eventInfo
                     .map((item) => {
                         const date = new Date(item.timestamp)
@@ -386,8 +406,6 @@ const Events = (props) => {
         </div>
     )
 }
-
-
 
 const Card = ({ children }) => {
     return (
